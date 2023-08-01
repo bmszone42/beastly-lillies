71440d6040df5008dc826279383e7ecd70f4859d
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-08-01 19:54:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,30 @@
-from collections import namedtuple
-import altair as alt
-import math
+import yfinance as yf
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+st.title('Stock Data')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Get stock symbol from user
+symbol = st.text_input('Enter Stock Symbol', 'AAPL') 
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Fetch data
+ticker = yf.Ticker(symbol)
+info = ticker.info
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Display data
+st.header(symbol + ' Data')
+st.markdown('**Closing Price:** ' + str(info['previousClose'])) 
+st.markdown('**52 Week High:** ' + str(info['fiftyTwoWeekHigh']))
+st.markdown('**52 Week Low:** ' + str(info['fiftyTwoWeekLow'])) 
+st.markdown('**1 Year Target Estimate:** ' + str(info['targetMeanPrice']))
+st.markdown('**Dividend:** ' + str(info['dividendRate'])) 
+st.markdown('**Yield:** ' + str(info['dividendYield'] * 100) + '%')
+st.markdown('**Average 30 Day Volume:** ' + str(info['averageVolume30days']))
 
+# Fetch historical data
+hist = ticker.history(period="1y") 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Display historical data
+st.header('Historical Data')
+st.line_chart(hist.Close)
+st.dataframe(hist)
