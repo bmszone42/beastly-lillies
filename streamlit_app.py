305338491a3913907512f1b2b0c108500278b866
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-08-01 21:10:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,29 @@
+import yfinance as yf
 import streamlit as st
-import yfinance as yf 
 import pandas as pd
-import plotly.express as px
 
-st.title('Dividend Stock Analysis')
+st.title('Dividend Investing Analysis')
 
-symbol = st.text_input('Enter a stock symbol', 'MSFT')
-years = st.slider('Select number of years', 1, 10, 5)
+tickers = st.text_input('Enter stock tickers separated by spaces')
 
-@st.cache
-def get_data(symbol):
-    return yf.download(symbol, period='5y')
-
-df = get_data(symbol, dividends=True)
-
-dividends = df[['Dividends']]
-prices = df[['Close']]
-
-def calc_recovery(divs, prices):
-    results = []
-    for i, row in dividends.iterrows():
-        div_amt = row['Dividends']
-        price = prices.loc[i]['Close']
-        c1 = price + 0.5 * div_amt 
-        c2 = price + 0.75 * div_amt
-        c3 = price + div_amt
+if tickers:
+    tickers = tickers.split()
+    
+    data = {}
+    metrics = {}
+    
+    for ticker in tickers:
+        data[ticker] = yf.Ticker(ticker)
+        history = data[ticker].history(period="max")
         
-        days = []
-        for c in [c1, c2, c3]:
-            mask = prices > c
-            days.append((mask[mask].index[0] - i).days)
-        results.append({'Date':i.date(), 'DaysToRecover': days})
-
-    return pd.DataFrame(results)
-
-recovery_data = calc_recovery(dividends, prices)
-
-st.subheader('Dividend Recovery Analysis')
-st.write(recovery_data)
-
-st.subheader('Stock Closing Price')
-fig = px.line(prices.reset_index(), x='Date', y='Close')
-st.write(fig)
+        metrics[ticker] = {
+            '52 Week High': history['High'].max(),
+            '52 Week Low': history['Low'].min(),
+            'Dividend': data[ticker].info['dividendRate'], 
+            'Yield': data[ticker].info['dividendYield'] * 100,
+            'Volume': history['Volume'].mean()
+        }
+        
+    metrics_df = pd.DataFrame(metrics).T
+    
+    st.table(metrics_df)
