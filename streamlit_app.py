a0f4229542d6f11597a4bdb77b87e6b6b2ef3e75
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-08-05 11:14:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,64 +1,76 @@
 import yfinance as yf
 import pandas as pd
 from datetime import datetime, timedelta
-
-# Define the stock and dividend dates
-stock_symbol = 'AAPL'  # Change this to your stock symbol
-dividend_dates = ['2023-04-01', '2022-04-01', '2020-04-01', '2019-04-01']  # add more dates as needed
-
-# Get the stock data
-stock = yf.Ticker(stock_symbol)
-hist = stock.history(period='10y')
+import streamlit as st
 
 # Function to check if dividends have been increasing over the past 10 years
 def is_increasing(series):
     return all(x<y for x, y in zip(series, series[1:]))
 
-dividends = hist[hist['Dividends'] > 0]['Dividends'].resample('Y').sum()
-if not is_increasing(dividends):
-    print(f"The dividends of {stock_symbol} have not been consistently increasing over the past 10 years.")
-    proceed = input("Do you wish to proceed? (y/n) ")
-    if proceed.lower() != 'y':
-        exit()
+def calculate(stock_symbol, proceed, dividend_dates, years_history):
+    # Get the stock data
+    stock = yf.Ticker(stock_symbol)
+    hist = stock.history(period=f'{years_history}y')
 
-results = []
+    dividends = hist[hist['Dividends'] > 0]['Dividends'].resample('Y').sum()
 
-# Loop through each dividend date
-for div_date_str in dividend_dates:
-    div_date = datetime.strptime(div_date_str, '%Y-%m-%d').date()
-    start_date = div_date - timedelta(days=10)
-    end_date = div_date + timedelta(days=90)
+    if not is_increasing(dividends) and proceed == 'No':
+        st.write(f"The dividends of {stock_symbol} have not been consistently increasing over the past {years_history} years.")
+    else:
+        # Proceed with calculations
+        st.write(f"Calculating for {stock_symbol}...")
+        results = []
+        # Loop through each dividend date
+        for div_date_str in dividend_dates:
+            div_date = datetime.strptime(div_date_str, '%Y-%m-%d').date()
+            start_date = div_date - timedelta(days=10)
+            end_date = div_date + timedelta(days=90)
 
-    # Get the window data
-    window_data = hist.loc[start_date:end_date]
+            # Get the window data
+            window_data = hist.loc[start_date:end_date]
 
-    if div_date in hist.index:  
-        # Get the dividend
-        dividend = hist.loc[div_date, 'Dividends']
+            if div_date in hist.index:  
+                # Get the dividend
+                dividend = hist.loc[div_date, 'Dividends']
 
-        # Get the opening price 10 days before the ex-date
-        opening_price = hist.loc[start_date, 'Open']
+                # Get the opening price 10 days before the ex-date
+                opening_price = hist.loc[start_date, 'Open']
 
-        # Calculate targets
-        targets = [(opening_price + dividend) * x for x in [1.5, 1.75, 2.0]]
+                # Calculate targets
+                targets = [(opening_price + dividend) * x for x in [1.5, 1.75, 2.0]]
 
-        target_days = {}
-        # Loop through each target
-        for i, target in enumerate(targets):
-            # Find the first day where the closing price is greater than or equal to the target
-            target_day = window_data[window_data['Close'] >= target].index.min()
-            if pd.notna(target_day):
-                target_days[f"{50*(i+1)}_target_days"] = (target_day - start_date).days
+                target_days = {}
+                # Loop through each target
+                for i, target in enumerate(targets):
+                    # Find the first day where the closing price is greater than or equal to the target
+                    target_day = window_data[window_data['Close'] >= target].index.min()
+                    if pd.notna(target_day):
+                        target_days[f"{50*(i+1)}_target_days"] = (target_day - start_date).days
 
-        # Calculate average
-        if target_days.values():
-            average_days = sum(target_days.values()) / len(target_days.values())
-        else:
-            average_days = None
+                # Calculate average
+                if target_days.values():
+                    average_days = sum(target_days.values()) / len(target_days.values())
+                else:
+                    average_days = None
 
-        target_days.update({'div_date': div_date, 'average_days': average_days})
-        results.append(target_days)
+                target_days.update({'div_date': div_date, 'average_days': average_days})
+                results.append(target_days)
 
-# Convert results to a DataFrame for easier viewing
-results_df = pd.DataFrame(results)
-print(results_df)
+        # Convert results to a DataFrame for easier viewing
+        results_df = pd.DataFrame(results)
+        st.write(results_df)
+
+def main():
+    # Define the stock and dividend dates
+    stock_symbol = st.sidebar.text_input("Enter stock symbol", 'AAPL')
+    proceed = st.sidebar.selectbox("Proceed if dividends are not increasing?", ('Yes', 'No'))
+    dividend_dates = st.sidebar.text_area("Enter dividend dates (YYYY-MM-DD), separated by commas", '2023-04-01,2022-04-01,2020-04-01,2019-04-01')
+    years_history = st.sidebar.slider("Select range for historical data (years)", 1, 20, 10)
+
+    # Convert dividend_dates from string to list of date strings
+    dividend_dates = [date.strip() for date in dividend_dates.split(',')]
+    
+    calculate(stock_symbol, proceed, dividend_dates, years_history)
+
+if __name__ == "__main__":
+    main()
