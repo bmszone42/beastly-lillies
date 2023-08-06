5b4a3e708a2c464cde05d64acbd7dabf4220138c
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-08-06 11:02:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,74 +1,86 @@
 import yfinance as yf
 import pandas as pd
 import streamlit as st
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 def calculate(stock_symbol, proceed, years_history):
-    if not proceed:
-        st.warning('The stock does not have an increasing dividend over the past 10 years.')
-        return
 
-    stock = yf.Ticker(stock_symbol)
-    hist = stock.history(period=f'{years_history}y')
-    dividends = stock.dividends
+  if not proceed:
+    st.warning('The stock does not have an increasing dividend over the past 10 years.')  
+    return
 
-    # Ensure dividends are in ascending order
-    dividends = dividends.sort_index(ascending=True)
+  stock = yf.Ticker(stock_symbol)
+  hist = stock.history(period=f'{years_history}y')
+  
+  dividends = stock.dividends
+  # Convert index to datetime
+  dividends.index = dividends.index.map(lambda x: datetime.strptime(str(x), '%Y%m%d'))
+  
+  results = []
 
-    results = []
+  for div_date, dividend in dividends.iterrows():
+    start_date = div_date - timedelta(days=10)
 
-    for div_date, dividend in dividends.items():
-        start_date = div_date - timedelta(days=10)
-        # Ensure start_date is a trading day
-        while start_date not in hist.index:
-            start_date -= timedelta(days=1)
-            if start_date < hist.index.min():
-                start_date = hist.index.min()
-                break
+    # Ensure start_date is a trading day
+    while start_date not in hist.index:
+      start_date -= timedelta(days=1)
 
-        opening_price = hist.loc[start_date, 'Open']
-        targets = {
-            '50%': opening_price + dividend * 0.5,
-            '75%': opening_price + dividend * 0.75,
-            '100%': opening_price + dividend * 1.0
-        }
-        
-        target_dates = {key: None for key in targets.keys()}
-        
-        window_data = hist.loc[start_date:div_date + timedelta(days=90)]
-        
-        for date, row in window_data.iterrows():
-            for key, target in targets.items():
-                if row['Open'] >= target and target_dates[key] is None:
-                    target_dates[key] = date
+    if start_date < hist.index.min():
+      start_date = hist.index.min()  
+      break
 
-        result_row = {
-            'Dividend Date': div_date,
-            'Opening Price': opening_price,
-            '50% Target': targets['50%'],
-            '75% Target': targets['75%'],
-            '100% Target': targets['100%'],
-            '50% Achieved': target_dates['50%'],
-            '75% Achieved': target_dates['75%'],
-            '100% Achieved': target_dates['100%'],
-        }
-        results.append(result_row)
+    opening_price = hist.loc[start_date, 'Open']
+    
+    price_on_dividend_date = hist.loc[div_date, 'Open']
+    
+    targets = {
+      '50%': opening_price + dividend * 0.5,
+      '75%': opening_price + dividend * 0.75,  
+      '100%': opening_price + dividend * 1.0
+    }
 
-    results_df = pd.DataFrame(results)
-    st.dataframe(results_df)
+    target_dates = {key: None for key in targets.keys()}
+
+    window_data = hist.loc[start_date:div_date + timedelta(days=90)]
+
+    for date, row in window_data.iterrows():
+      for key, target in targets.items():
+        if row['Open'] >= target and target_dates[key] is None:
+          target_dates[key] = date
+
+    result_row = {
+      'Dividend Date': div_date.strftime('%Y-%m-%d'),
+      'Opening Date': start_date.strftime('%Y-%m-%d'),
+      'Price on Dividend Date': price_on_dividend_date,
+      'Opening Price': opening_price,
+      '50% Target': targets['50%'],
+      '75% Target': targets['75%'],
+      '100% Target': targets['100%'],
+      '50% Achieved': target_dates['50%'].strftime('%Y-%m-%d') if target_dates['50%'] else None,
+      '75% Achieved': target_dates['75%'].strftime('%Y-%m-%d') if target_dates['75%'] else None,
+      '100% Achieved': target_dates['100%'].strftime('%Y-%m-%d') if target_dates['100%'] else None,
+    }
+
+    results.append(result_row)
+
+  results_df = pd.DataFrame(results)
+  st.dataframe(results_df)
 
 def main():
-    stock_symbol = st.sidebar.text_input('Enter stock symbol:', 'STOCK_SYMBOL')
-    years_history = st.sidebar.slider('Select number of years for history:', min_value=3, max_value=20, value=3)
-    proceed_button = st.sidebar.button('Execute')
 
-    # Check if dividends are increasing over the past 10 years
-    stock = yf.Ticker(stock_symbol)
-    dividends = stock.dividends
-    proceed = dividends.is_monotonic_increasing
+  stock_symbol = st.sidebar.text_input('Enter stock symbol:', 'AAPL')
 
-    if proceed_button:
-        calculate(stock_symbol, proceed, years_history)
+  years_history = st.sidebar.slider('Select number of years for history:', min_value=5, max_value=20, value=10)  
+
+  proceed_button = st.sidebar.button('Execute')
+
+  # Check if dividends are increasing over the past 10 years
+  stock = yf.Ticker(stock_symbol)
+  dividends = stock.dividends
+  proceed = dividends.is_monotonic_increasing
+
+  if proceed_button:
+    calculate(stock_symbol, proceed, years_history)
 
 if __name__ == "__main__":
-    main()
+  main()
