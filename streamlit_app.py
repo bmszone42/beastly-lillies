ad4b20828e2ce86a403e0f7cf4e27f528b52b901
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-08-06 17:04:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,76 +1,85 @@
-import yfinance as yf
-import pandas as pd
-import streamlit as st
+"""Streamlit app to analyze stock dividend performance."""
+
 from datetime import datetime, timedelta
+import logging
 
-def calculate(stock_symbol, proceed, years_history):
+import pandas as pd
+import yfinance as yf
+import streamlit as st
 
-  if not proceed:
-    st.warning('The stock does not have an increasing dividend over the past 10 years.')  
-    return
+# Constants
+DEFAULT_YEARS = 3  
+MAX_YEARS = 20
+TARGET_PERCENTS = [0.5, 0.75, 1.0]   
 
-  stock = yf.Ticker(stock_symbol)
-  hist = stock.history(period=f'{years_history}y')
+logging.basicConfig(level=logging.INFO)
+
+def get_historical_data(symbol, years):
+  ticker = yf.Ticker(symbol)    
+  start = datetime.now() - timedelta(days=365*years)
+  data = ticker.history(start=start)
+  return data
+
+def calculate_target_prices(dividend, opening_price):
+  targets = {
+      f"{p*100}%": opening_price + dividend * p   
+      for p in TARGET_PERCENTS
+  }
+  return targets
+
+def find_target_dates(targets, data):
+  target_dates = {p:None for p in targets.keys()}
+  for date, row in data.iterrows():
+    open_price = row['Open']    
+    for p, target in targets.items():
+      if open_price >= target and target_dates[p] is None:
+        target_dates[p] = date
+        
+  return target_dates
   
-  dividends = stock.dividends
-  # Convert index to datetime
-  dividends.index = dividends.index.map(lambda x: datetime.strptime(str(x), '%Y%m%d'))
+def analyze_dividends(symbol, years=DEFAULT_YEARS):
   
+  data = get_historical_data(symbol, years)
+  dividends = get_dividends(symbol) #mocked for example
+
   results = []
 
-  for div_date, dividend in dividends.iterrows():
-    start_date = div_date - timedelta(days=10)
+  for dividend_date, dividend in dividends.iteritems():
+    start_date = get_first_valid_date(dividend_date, data)
+    open_price = data.loc[start_date, 'Open']
+    price_on_dividend_date = data.loc[dividend_date, 'Open']
 
-    # Ensure start_date is a trading day
-    while start_date not in hist.index:
-      start_date -= timedelta(days=1)
+    targets = calculate_target_prices(dividend, open_price)
+    target_dates = find_target_dates(targets, data)
 
-    if start_date < hist.index.min():
-      start_date = hist.index.min()  
-      break
-
-    opening_price = hist.loc[start_date, 'Open']
-    
-    price_on_dividend_date = hist.loc[div_date, 'Open']
-    
-    targets = {
-      '50%': opening_price + dividend * 0.5,
-      '75%': opening_price + dividend * 0.75,  
-      '100%': opening_price + dividend * 1.0
-    }
-
-    target_dates = {key: None for key in targets.keys()}
-
-    window_data = hist.loc[start_date:div_date + timedelta(days=90)]
-
-    for date, row in window_data.iterrows():
-      for key, target in targets.items():
-        if row['Open'] >= target and target_dates[key] is None:
-          target_dates[key] = date
+    pcts_met = {p: date is not None for p, date in target_dates.items()}
+    days_to_targets = {p: (target_dates[p] - dividend_date).days  
+                       for p, date in target_dates.items() if date}
 
     result_row = {
-      'Dividend Date': div_date.strftime('%Y-%m-%d'),
-      'Opening Date': start_date.strftime('%Y-%m-%d'),
-      'Price on Dividend Date': price_on_dividend_date,
-      'Opening Price': opening_price,
-      '50% Target': targets['50%'],
-      '75% Target': targets['75%'],
-      '100% Target': targets['100%'],
-      '50% Achieved': target_dates['50%'].strftime('%Y-%m-%d') if target_dates['50%'] else None,
-      '75% Achieved': target_dates['75%'].strftime('%Y-%m-%d') if target_dates['75%'] else None,
-      '100% Achieved': target_dates['100%'].strftime('%Y-%m-%d') if target_dates['100%'] else None,
+      "Dividend Date": dividend_date,
+      "Opening Date": start_date,
+      "Opening Price": open_price,
+      "Price on Dividend Date": price_on_dividend_date,
+      "50% Target": targets['50%'],
+      "75% Target": targets['75%'], 
+      "100% Target": targets['100%'],
+      "50% Target Date": target_dates['50%'],
+      "75% Target Date": target_dates['75%'],
+      "100% Target Date": target_dates['100%'],
+      "Percent Targets Met": pcts_met,
+      "Days to Meet Targets": days_to_targets
     }
 
     results.append(result_row)
 
-  results_df = pd.DataFrame(results)
-  st.dataframe(results_df)
+  return pd.DataFrame(results)
 
 def main():
 
-  stock_symbol = st.sidebar.text_input('Enter stock symbol:', 'STOCK_SYMBOL')
+  stock_symbol = st.sidebar.text_input('Enter stock symbol:', 'AAPL')
 
-  years_history = st.sidebar.slider('Select number of years for history:', min_value=3, max_value=20, value=3)  
+  years_history = st.sidebar.slider('Select number of years for history:', min_value=10, max_value=20, value=10)  
 
   proceed_button = st.sidebar.button('Execute')
 
