30d52271682657418134a8f0e9472bc4990ce7b3
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-08-07 21:51:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,7 +79,10 @@ def calculate_avg_days(symbols, date, years):
   st.write(f'Data info for {symbols}')
   st.write(data)
   
-  dividend_dates = pd.concat(data)
+  #dividend_dates = pd.concat(data)
+  # Convert 'Dividend Date' column to datetime type explicitly
+  dividend_dates['Dividend Date'] = pd.to_datetime(dividend_dates['Dividend Date'])
+
 
   st.write(f'Dividend info for {symbols}')
   st.write(dividend_dates)
