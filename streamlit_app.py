c1ebf5b4029b66acbb7f7d3ecd23680cf7e6b73a
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-08-09 10:25:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,101 +3,102 @@ import pandas as pd
 import streamlit as st
 from datetime import datetime, timedelta
 
-def get_stock_data(symbol, years):
+# Function to check if a stock is ex-dividend today
+def is_ex_dividend_today(symbol):
     try:
         stock = yf.Ticker(symbol)
         dividends = stock.dividends
         today = pd.Timestamp.today().date()
-        closest_ex_dividend_date = dividends.index[dividends.index <= today].max()
+        return today in dividends.index
+    except Exception:
+        return False
 
-        if pd.isna(closest_ex_dividend_date):
-            ex_dividend_message = "No past or present ex-dividend date found."
-        else:
-            next_dividend_date = dividends.index[dividends.index > today].min()
-            ex_dividend_message = f"Closest ex-dividend date: {closest_ex_dividend_date.strftime('%Y-%m-%d')}."
+# Function to get detailed dividend data for a symbol
+def get_symbol_data(symbol, years):
+    stock = yf.Ticker(symbol)
+    hist = stock.history(period=f'{years}y')
 
-            if pd.notna(next_dividend_date):
-                ex_dividend_message += f" Next ex-dividend date: {next_dividend_date.strftime('%Y-%m-%d')}."
+    if 'Dividends' not in hist.columns:
+        return pd.DataFrame()
 
-        is_ex_dividend_today = today == closest_ex_dividend_date
+    dividend_dates = []
 
-        # Display the dividends data as a table in Streamlit
-        st.write(f"Dividend data for {symbol}:")
-        st.write(dividends.to_frame())
+    for i, dividend in hist['Dividends'].items():
+        if dividend > 0:
+            try:
+                prev_date = i - timedelta(days=10)
+                next_date = i + timedelta(days=60)
 
-        hist = stock.history(period=f'{years}y')
-        # Display the history data as a table in Streamlit
-        st.write(f"History data for {symbol}:")
-        st.write(hist.to_frame())
+                if prev_date in hist.index and next_date in hist.index:
+                    prev_price = hist.loc[prev_date, 'Open']
+                    next_price = hist.loc[next_date, 'Open']
 
-        # Continue with the logic from get_symbol_data
-        dividend_dates = []
+                    percentage_increase = ((next_price - prev_price) / prev_price) * 100
+                    target = dividend + prev_price
 
-        for i, dividend in hist['Dividends'].items():
-            if dividend > 0:
-                try:
-                    prev_date = i - timedelta(days=10)
-                    next_date = i + timedelta(days=60)
+                    days_to_target = ((hist.loc[next_date:, 'Open'] >= target).idxmax() - next_date).days
 
-                    if prev_date in hist.index and next_date in hist.index:
-                        prev_price = hist.loc[prev_date, 'Open']
-                        next_price = hist.loc[next_date, 'Open']
+                    dividend_dates.append({
+                        'Symbol': symbol,
+                        'Dividend Date': i.strftime('%Y-%m-%d'),
+                        'Ex-Dividend Date': stock.dividends[stock.dividends.index <= i][-1].strftime('%Y-%m-%d'),
+                        'Month': i.month,
+                        'Dividend Amount': dividend,
+                        'Price on Dividend Date': hist.loc[i, 'Close'],
+                        '-10 Days Date': prev_date.strftime('%Y-%m-%d'),
+                        'Opening Price -10 Days': prev_price,
+                        '+60 Days Date': next_date.strftime('%Y-%m-%d'),
+                        'Opening Price +60 Days': next_price,
+                        '% Increase': round(percentage_increase, 1),
+                        'Target': target,
+                        'Date Used for Target': prev_date.strftime('%Y-%m-%d'),
+                        'Days to Opening Price > Target': days_to_target
+                    })
 
-                        percentage_increase = ((next_price - prev_price) / prev_price) * 100
-                        target = dividend + prev_price
+            except KeyError:
+                continue
 
-                        days_to_target = ((hist.loc[next_date:, 'Open'] >= target).idxmax() - next_date).days
-
-                        dividend_dates.append({
-                            'Symbol': symbol,
-                            'Dividend Date': i.strftime('%Y-%m-%d'),
-                            'Ex-Dividend Date': stock.dividends.index[stock.dividends.index <= i][-1].strftime('%Y-%m-%d'),
-                            'Month': i.month,
-                            'Dividend Amount': dividend,
-                            'Price on Dividend Date': hist.loc[i, 'Close'],
-                            '-10 Days Date': prev_date.strftime('%Y-%m-%d'),
-                            'Opening Price -10 Days': prev_price,
-                            '+60 Days Date': next_date.strftime('%Y-%m-%d'),
-                            'Opening Price +60 Days': next_price,
-                            '% Increase': round(percentage_increase, 1),
-                            'Target': target,
-                            'Date Used for Target': prev_date.strftime('%Y-%m-%d'),
-                            'Days to Opening Price > Target': days_to_target
-                        })
-
-                except KeyError:
-                    continue
-
-        return is_ex_dividend_today, ex_dividend_message, pd.DataFrame(dividend_dates)
-
-    except Exception as e:
-        return False, f"Error fetching dividend data for {symbol}: {e}", None
+    return pd.DataFrame(dividend_dates)
 
 # Function to calculate average days
 def calculate_avg_days(symbols, years):
     data = []
+
     for symbol in symbols:
-        _, _, dividend_data = get_stock_data(symbol, years)
-        data.append(dividend_data)
+        data.append(get_symbol_data(symbol, years))
 
     dividend_dates = pd.concat(data)
+
     dividend_dates['Dividend Date'] = pd.to_datetime(dividend_dates['Dividend Date'])
     dividend_dates['Ex-Dividend Date'] = pd.to_datetime(dividend_dates['Ex-Dividend Date'])
 
     grouped = dividend_dates.groupby('Month')
+
     avg_days = []
 
     for month, group in grouped:
         periods = [group[i:i+10] for i in range(0, len(group), 10)]
         for period in periods:
             avg = period['Days to Opening Price > Target'].mean()
-            avg_days.append({'Month': month, 'Avg Days': avg})
+            avg_days.append({'Symbol': symbol, 'Month': month, 'Avg Days': avg})
 
     avg_days_df = pd.DataFrame(avg_days)
+
     return dividend_dates, avg_days_df
+
+def display_sidebar(valid_symbols):
+    st.sidebar.markdown("## Ex-Dividend Today")
+    for symbol in valid_symbols:
+        ex_dividend_today = is_ex_dividend_today(symbol)
+        st.sidebar.write(f'{symbol}: Ex-Dividend Today? {ex_dividend_today}')
 
 def main():
     st.title("Stock Dividend Analysis")
+    st.markdown("## Instructions")
+    st.write("1. Enter stock symbols separated by commas.")
+    st.write("2. Choose a dividend date.")
+    st.write("3. Click the 'Calculate' button to analyze dividend data.")
+
     years = 10
     valid_symbols = []
 
@@ -114,25 +115,32 @@ def main():
         symbols = symbols.split(',')
         for symbol in symbols:
             symbol = symbol.strip().upper()
-            is_ex_dividend, message, _ = get_stock_data(symbol, years)
-            st.sidebar.write(f'{symbol}: {message}')
-            valid_symbols.append(symbol)
-
-        try:
-            dividend_dates, avg_days_df = calculate_avg_days(valid_symbols, years)
-            if show_detailed_results:
-                st.subheader(f'Dividend info for {valid_symbols}')
-                st.write(dividend_dates)
-                st.subheader(f'Average Days for {valid_symbols}')
-                st.write(avg_days_df)
+            if yf.Ticker(symbol).info:
+                valid_symbols.append(symbol)
             else:
-                summarized_results = dividend_dates[['Symbol', 'Ex-Dividend Date', 'Days to Opening Price > Target']].copy()
-                summarized_results = summarized_results.drop_duplicates(subset=['Symbol', 'Ex-Dividend Date'])
-                st.subheader('Summarized Results')
-                st.write(summarized_results)
+                st.error(f"Invalid stock symbol: {symbol}")
 
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
+        if valid_symbols:
+            try:
+                dividend_dates, avg_days_df = calculate_avg_days(valid_symbols, years)
+                if show_detailed_results:
+                    st.subheader(f'Dividend info for {valid_symbols}')
+                    st.write(dividend_dates)
+
+                    st.subheader(f'Average Days for {valid_symbols}')
+                    st.write(avg_days_df)
+                else:
+                    summarized_results = dividend_dates[['Symbol', 'Ex-Dividend Date', 'Days to Opening Price > Target']].copy()
+                    summarized_results = summarized_results.drop_duplicates(subset=['Symbol', 'Ex-Dividend Date'])
+                    st.subheader('Summarized Results')
+                    st.write(summarized_results)
+
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
+
+            display_sidebar(valid_symbols)
+        else:
+            st.error("No valid stock symbols provided.")
 
 if __name__ == '__main__':
     main()
