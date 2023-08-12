dd3e20676c3d16ce70689e8bbce90005a3b644f0
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-08-11 20:17:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -141,17 +141,20 @@ def main():
                     # Create summarized results dataframe 
                     summarized_results['Avg Days'] = summarized_results.groupby('Symbol')['Days to Opening Price > Target'].transform('mean')
                     
+                    # Create summarized results dataframe 
+
+                    # Add Avg Days column
+                    summarized_results['Avg Days'] = summarized_results.groupby('Symbol')['Days to Opening Price > Target'].transform('mean') 
+                    
                     # Round Avg Days column
-                    summarized_results['Avg Days'] = summarized_results['Avg Days'].round(1) 
+                    summarized_results['Avg Days'] = summarized_results['Avg Days'].round(1)
                     
-                    # Display header
-                    st.subheader('Summarized Results')
-                    
-                    # Display data
-                    st.table(data=summarized_results[['Symbol', 'Ex-Dividend Date','Avg Days','Days to Opening Price > Target']], columns_name=['**Symbol**','**Ex-Dividend Date**','**Avg Days**','**Days to Target**'])                    # for symbol, group in summarized_results.groupby('Symbol'):
-                    #     st.write(f"Symbol: {symbol}")
-                    #     st.write(f"Avg Days: {round(group['Avg Days'].iloc[0], 1)}")
-                        #st.write(group)
+                    # Display results
+                    for symbol, group in summarized_results.groupby('Symbol'):
+                        
+                        st.subheader(symbol)
+                        
+                        st.table(group[['Ex-Dividend Date', 'Avg Days', 'Days to Opening Price > Target']])
             except Exception as e:
                 #st.error(f"An error occurred: {e}")
                 print(f"An error occurred: {e}")
