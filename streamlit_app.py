8190db8ebf65a24fa5c987d5ee6617d52ada9d4e
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-08-11 20:09:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -141,14 +141,14 @@ def main():
                     # Add this after the summarized_results dataframe is created
                     summarized_results['Avg Days'] = summarized_results.groupby('Symbol')['Days to Opening Price > Target'].transform('mean')
 
-            for symbol, group in summarized_results.groupby('Symbol'):
-
-                st.subheader(f"{symbol}")
-            
-                color = 'green' if group['Avg Days'].iloc[0] <= 30 else 'red'
-                st.write(f"<span style='color:{color}'>Avg Days: {round(group['Avg Days'].iloc[0], 1)}</span>", unsafe_allow_html=True)
-            
-                st.table(data=group[['Ex-Dividend Date', 'Days to Opening Price > Target']], columns_name=['**Ex-Dividend Date**','**Days to Target**'], index=False)
+                    for symbol, group in summarized_results.groupby('Symbol'):
+        
+                        st.subheader(f"{symbol}")
+                    
+                        color = 'green' if group['Avg Days'].iloc[0] <= 30 else 'red'
+                        st.write(f"<span style='color:{color}'>Avg Days: {round(group['Avg Days'].iloc[0], 1)}</span>", unsafe_allow_html=True)
+                    
+                        st.table(data=group[['Ex-Dividend Date', 'Days to Opening Price > Target']], columns_name=['**Ex-Dividend Date**','**Days to Target**'], index=False)
                     # for symbol, group in summarized_results.groupby('Symbol'):
                     #     st.write(f"Symbol: {symbol}")
                     #     st.write(f"Avg Days: {round(group['Avg Days'].iloc[0], 1)}")
